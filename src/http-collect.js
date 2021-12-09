cfb4903f22b6ccc18931fbc1a7725893e8ee9dde
Mick Piereder <mick.io@pm.me>
2021-12-09 12:08:35 -0700

    completed http-collect

---
+++ src/http-collect.js
@@ -0,0 +1,12 @@
+const http = require("http");
+
+const url = process.argv[2];
+
+http.get(url, (response) => {
+  let data = "";
+  response.on("data", (chunk) => (data += chunk));
+  response.on("end", () => {
+    console.log(data.length);
+    console.log(data);
+  });
+});
